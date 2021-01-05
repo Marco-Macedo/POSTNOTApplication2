25e6e3b968a3ee215cc4e186ea60eac8f34fda88
Marco-Macedo <marcomacedo@ipvc.pt>
2021-01-05 18:25:23 +0000

    last updates

--- app/src/main/java/com/example/postnotapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/postnotapplication/DashboardActivity.kt
@@ -1,12 +1,20 @@
 package com.example.postnotapplication
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -17,32 +25,35 @@ import java.lang.StringBuilder
 
 class DashboardActivity : AppCompatActivity() {
 
-    private val CHANNEL_ID = "cha"
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationId = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         var database = FirebaseDatabase.getInstance().reference
         //database.setValue("entrei")
 
+        createNotificationChannel()     // chama a funcao createNOtificationChannel
+
         var getdata = object : ValueEventListener {
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
 
-
-
             override fun onDataChange(snapshot: DataSnapshot) {
                 var sb = StringBuilder()
                 for(i in snapshot.children){
-                    var state = i.child("ledstate").getValue()
+                    var state = i.child("caixadecorreio").getValue()
                     sb.append("Estado: $state")
-                    if(state == "Ligado")
+                    if(state == "Aberta")
                     {
-                        Toast.makeText(
+                       Toast.makeText(
                             baseContext,
-                            "LED ESTÁ LIGADA",
+                            "CORREIO FOI ABERTO",
                             Toast.LENGTH_SHORT
                         ).show()
+
+                        sendNotification()
                     }
                 }
                 state.setText(sb)
@@ -53,6 +64,46 @@ class DashboardActivity : AppCompatActivity() {
         }
         database.addValueEventListener(getdata)
         database.addListenerForSingleValueEvent(getdata)
+
+
+    }
+
+    private fun sendNotification() {
+
+        // imagem
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val bitmap = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.caixacorreio)
+
+        // send notification
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Caixa de Correio")
+            .setContentText("ABERTA")
+            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
